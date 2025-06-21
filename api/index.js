05f0aa5db0a414b8ee07c2b77f5c6122062d0895
MarvinEgoavil <marvinegoavilz@gmail.com>
2025-06-21 12:22:57 +0200

    Probando nuevo codigo en index.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-let mensajes = [];
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('¡Funciona en Railway! 🚂');
 });
 
-app.get('/mensajes', (req, res) => {
-  res.json(mensajes);
+app.listen(PORT, () => {
+  console.log('API corriendo en el puerto', PORT);
 });
-
-
-app.post('/contacto', (req, res) => {
-  const { nombre, email, mensaje } = req.body;
-  if (!nombre || !email || !mensaje) {
-    return res.status(400).json({ error: 'Todos los campos son obligatorios.' });
-  }
-  mensajes.push({ nombre, email, mensaje, fecha: new Date() });
-  res.json({ mensaje: '¡Mensaje recibido (sin email)!' });
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`API corriendo en el puerto ${PORT}`));
