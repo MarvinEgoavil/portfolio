8411fc9b240de53bde08e334640b948cbeb3b101
MarvinEgoavil <marvinegoavilz@gmail.com>
2025-06-22 07:59:56 +0200

    modifique main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,8 @@ import { iniciarIdioma } from './idiomas.js';
 import { iniciarVantaFondo } from './vanta-config.js';
 import { iniciarFirebase } from './firebase.js';
 import { inicializarMapaContacto } from './mapa.js';  
-import { inicializarFormularioContacto} from './formulario.js';
+import { inicializarFormularioContacto } from './formulario.js';
 
-
-// 🚀 INICIALIZACIÓN AL CARGAR DOM
 document.addEventListener("DOMContentLoaded", () => {
   configurarSidebarMovil();
   configurarEmojiGuiñador();
@@ -24,46 +22,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const placeholderOriginal = buscador ? buscador.placeholder : '';
 
   if (buscador) {
-    // Cuando haces focus o hover: borra el placeholder
     function borrarPlaceholder() {
       buscador.placeholder = '';
     }
     buscador.addEventListener('focus', borrarPlaceholder);
     buscador.addEventListener('mouseenter', borrarPlaceholder);
 
-    // Cuando quitas el focus: restaura si está vacío
     buscador.addEventListener('blur', function() {
       if (this.value.trim() === '') {
         this.placeholder = placeholderOriginal;
       }
     });
 
-    // Cuando sale el mouse y el campo está vacío: restaura también
     buscador.addEventListener('mouseleave', function() {
       if (document.activeElement !== buscador && this.value.trim() === '') {
         this.placeholder = placeholderOriginal;
       }
     });
   }
-});
-
-/* Capturando el token */ 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
-  const recaptchaInput = document.getElementById('recaptcha-token');
-
-  if (!form) return;
-
-  form.addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevenir envío normal
-
-    grecaptcha.ready(() => {
-      grecaptcha.execute('6Ld9TWkrAAAAAC_JVShrurJmr29KzKJ7pDSLajkH', {action: 'submit'})
-        .then(token => {
-          recaptchaInput.value = token; // Poner token en el input hidden
-          form.submit(); // Ahora sí envía el formulario con el token incluido
-        });
-    });
-  });
 });
